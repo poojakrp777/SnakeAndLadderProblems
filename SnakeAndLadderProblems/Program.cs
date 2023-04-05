8f8256414e60b58f9eb45b965e157e3fc7c0d372
poojakrp777 <poojakrp777@gmail.com>
2023-04-06 00:02:24 +0530

    [POOJA] Rolling die using a random function

--- SnakeAndLadderProblems/Program.cs
+++ SnakeAndLadderProblems/Program.cs
@@ -12,7 +12,15 @@ namespace SnakeAndLadder
         {
             Console.WriteLine("Welcome to snake and ladder programs");
             int StartPosition = 0;
+            //UC2 Rolling a dice
+            int ran = 0;
+            Random random = new Random(); //Random function can generate random number
+            ran = random.Next(1, 7);//Considering range from 1 to 6 i.e below 7
+            Console.WriteLine("Rolled Dice");
+            Console.WriteLine("Number got: " + ran);
+            //UC-Starting Position zero
             Console.WriteLine("Starting Position of Player 1 is :" + StartPosition);
+            Console.ReadLine();
         }
     }
 }
