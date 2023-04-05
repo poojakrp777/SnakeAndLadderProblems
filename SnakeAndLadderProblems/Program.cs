1e1982dc270565f34b65754e4da139be1d6f7920
poojakrp777 <poojakrp777@gmail.com>
2023-04-05 23:58:11 +0530

    [POOJA] Start Position in Zero

---
+++ SnakeAndLadderProblems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake and ladder programs");
+            int StartPosition = 0;
+            Console.WriteLine("Starting Position of Player 1 is :" + StartPosition);
+        }
+    }
+}
